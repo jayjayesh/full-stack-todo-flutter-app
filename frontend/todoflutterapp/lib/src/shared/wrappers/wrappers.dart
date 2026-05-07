cfd245f960221a55d28a84ae5edfc02b1912634f
jayjayesh <lathiyajayesh77@gmail.com>
2026-05-07 20:45:16 +0530

    feat: Add DismissKeyboardWrapper to dismiss keyboard on tap outside inputs

--- frontend/todoflutterapp/lib/src/shared/wrappers/wrappers.dart
+++ frontend/todoflutterapp/lib/src/shared/wrappers/wrappers.dart
@@ -2,5 +2,5 @@
 export 'skeleton_wrapper.dart';
 export 'localization_wrapper.dart';
 export 'state_wrapper.dart';
-
+export 'dismiss_keyboard_wrapper.dart';
 export 'session_listener_wrapper.dart';
